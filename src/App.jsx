7a26d45cf9f49536b9ec019abcb556f6f64d1c4b
DiegoCass01 <diego.castillo170901@gmail.com>
2024-11-26 20:48:12 -0600

    styling form - input and button

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,18 @@ import './App.css'
 function App() {
 
   return (
-    <h1>
-      Movie Searcher
-    </h1>
+    <div className='page'>
+      <header>
+        <h1>Movie Search</h1>
+        <form className="form">
+          <input placeholder='Avengers, Star Wars...' />
+          <button type="submit">Search</button>
+        </form>
+      </header>
+      <main>
+        Resultados
+      </main>
+    </div>
   )
 }
 
