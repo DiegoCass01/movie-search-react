80015ecd8069816477f8e830f6da53eea5acd323
DiegoCass01 <diego.castillo170901@gmail.com>
2024-11-27 00:35:16 -0600

    get info - FormData(event.target)

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import { useRef } from 'react'
 import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
 function App() {
   const { movies } = useMovies()
-  const inputRef = useRef()
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    const inpuEl = inputRef.current
-    const value = inpuEl.value
-    console.log(value);
+    const { query } = Object.fromEntries(
+      new window.FormData(event.target)
+    )
+    console.log(query);
 
   }
 
@@ -20,7 +19,7 @@ function App() {
       <header>
         <h1>Movie Search</h1>
         <form className="form" onSubmit={handleSubmit} >
-          <input ref={inputRef} placeholder='Avengers, Star Wars...' />
+          <input name='query' placeholder='Avengers, Star Wars...' />
           <button type="submit">Search</button>
         </form>
       </header>
