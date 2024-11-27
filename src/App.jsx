3120d69da6e9c205984af34f93be6710593094df
DiegoCass01 <diego.castillo170901@gmail.com>
2024-11-27 00:28:38 -0600

    useRef and handleSubmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
+import { useRef } from 'react'
 import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
 function App() {
   const { movies } = useMovies()
+  const inputRef = useRef()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const inpuEl = inputRef.current
+    const value = inpuEl.value
+    console.log(value);
+
+  }
 
   return (
     <div className='page'>
       <header>
         <h1>Movie Search</h1>
-        <form className="form">
-          <input placeholder='Avengers, Star Wars...' />
+        <form className="form" onSubmit={handleSubmit} >
+          <input ref={inputRef} placeholder='Avengers, Star Wars...' />
           <button type="submit">Search</button>
         </form>
       </header>
