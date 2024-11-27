a3b2b3e04d3842c734cae6b7b45d6754ae10a79b
DiegoCass01 <diego.castillo170901@gmail.com>
2024-11-27 00:53:26 -0600

    handling errors using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
@@ -6,6 +6,7 @@ import { useMovies } from './hooks/useMovies'
 function App() {
   const { movies } = useMovies()
   const [query, setQuery] = useState('')
+  const [error, setError] = useState(null)
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -19,6 +20,22 @@ function App() {
     setQuery(event.target.value)
   }
 
+  useEffect(() => {
+    if (query === '') {
+      setError('No se puede buscar una película vacía')
+      return
+    }
+    if (query.match(/^\d+$/)) {
+      setError('No se puede buscar una pelicula co un número')
+      return
+    }
+    if (query.length < 3) {
+      setError('La búsqueda debe tener al menos 3 caracteres')
+      return
+    }
+    setError(null)
+  }, [query])
+
   return (
     <div className='page'>
       <header>
@@ -27,6 +44,7 @@ function App() {
           <input onChange={handleChange} value={query} name='query' placeholder='Avengers, Star Wars...' />
           <button type="submit">Search</button>
         </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
       <main>
         <Movies movies={movies} />
