ff306063281fc2fd272171e14ccd910040a11716
DiegoCass01 <diego.castillo170901@gmail.com>
2024-12-12 23:22:48 -0600

    useCallback

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,4 +1,4 @@
-import { useRef, useState, useMemo } from "react";
+import { useRef, useState, useMemo, useCallback } from "react";
 import { searchMovies } from "../services/movies";
 
 export function useMovies({ search, sort }) {
@@ -7,21 +7,19 @@ export function useMovies({ search, sort }) {
   const [error, setError] = useState(null);
   const previousSearch = useRef(search);
 
-  const getMovies = useMemo(() => {
-    return async ({ search }) => {
-      if (search === previousSearch.current) return;
-      try {
-        setLoading(true);
-        setError(null);
-        previousSearch.current = search;
-        const newMovies = await searchMovies({ search });
-        setMovies(newMovies);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
+  const getMovies = useCallback(async ({ search }) => {
+    if (search === previousSearch.current) return;
+    try {
+      setLoading(true);
+      setError(null);
+      previousSearch.current = search;
+      const newMovies = await searchMovies({ search });
+      setMovies(newMovies);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
   }, []);
 
   // const sortedMovies = sort
